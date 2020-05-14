2da9bcdd39a321b16229b9b3e1a489905d4d9358
eteka03 <eteka03.akpaki@gmail.com>
2020-05-14 18:17:37 -0400

    some code and security reviews

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const bcrypt = require('bcrypt')
 const cors = require('cors')
 const knex = require('knex')
 
+//controllers
+const register = require('./controllers/register')
 
 //database connection
 const db = knex({
@@ -65,42 +68,7 @@ app.post('/signin' ,(req,res)=>{
 
 })
 
-app.post('/register',(req,res)=>{
-        const {email,pwd , name} = req.body;
-         const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(pwd, salt);
-
-        db.transaction(trx => {
-            trx.insert({
-                hash:hash,
-                email:email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-
-              return trx('users')
-                .returning('*')
-                .insert({
-                    email:loginEmail[0],
-                    name:name,
-                    joined:new Date()
-                })
-                .then(user =>{
-                    res.json(user[0])
-                    
-                })
-              
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-        .catch(err => res.status(400).json('unable to register'))
-        
-   
-        
-       
-})
+app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)})
 
 app.get('/profile/:id' ,(req,res)=>{
     const {id} = req.params
@@ -133,5 +101,5 @@ app.put('/image',(req,res)=>{
 
 
 app.listen(8000,()=>{
-    console.log('app running on port 8000')
+    console.log(`àpp runnin on port 8000 ${process.env.DB_HOST}`)
 })
